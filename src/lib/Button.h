d49b83c86990855e37866f412de0a8b6b7c67015
Akshay Nair <phenax5@gmail.com>
2024-11-03 17:50:01 +0530

    Remove Button.cpp because dumb

--- src/lib/Button.h
+++ src/lib/Button.h
@@ -1,21 +1,53 @@
 #pragma once
+#include <Arduino.h>
 
 #define DOUBLE_PRESS_INTERVAL 300
 
 class Button {
-private:
   int pin;
   int prevActive = 0;
   long lastPressTime = 0;
 
-  bool active();
+  bool active() { return digitalRead(this->pin) == 0; }
 
 public:
-  Button(int pin);
+  Button(int pin) { this->pin = pin; }
 
-  void setup();
-  void loop();
+  void setup() { pinMode(this->pin, INPUT_PULLUP); }
+  void loop() {}
 
-  bool isPressed();
-  short doublePressCount();
+  bool isPressed() {
+    bool isActive = this->active();
+    bool pressed = isActive && !this->prevActive;
+
+    this->prevActive = isActive;
+
+    return pressed;
+  }
+
+  short doublePressCount() {
+    bool isActive = this->active();
+    bool pressed = isActive && !this->prevActive;
+    unsigned long currentTime = millis();
+    this->prevActive = isActive;
+
+    if (!pressed) {
+      if (this->lastPressTime != 0 &&
+          currentTime - this->lastPressTime > DOUBLE_PRESS_INTERVAL) {
+        this->lastPressTime = 0;
+        return 1;
+      }
+
+      return 0;
+    }
+
+    if (currentTime - this->lastPressTime > DOUBLE_PRESS_INTERVAL) {
+      this->lastPressTime = currentTime;
+      return 0;
+    }
+
+    this->lastPressTime = 0;
+
+    return 2;
+  }
 };
